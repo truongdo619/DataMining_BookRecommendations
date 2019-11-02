0d88a1aff07f3853a9a3b107678aa8aabfec3b70
truongdo619 <conmuonvenha@gmail.com>
2019-11-02 13:25:13 +0700

    update

--- helper/preparing_dataset.py
+++ helper/preparing_dataset.py
@@ -14,10 +14,12 @@ def preparing(path, threshold = 20):
         if result is None:
             result = data[data[:, 0] == item]
         elif arr[item - 1] != 0:
+            print(item)
             tmp = data[data[:, 0] == item]
             for in_item in range(tmp.shape[0]):
+                print(tmp[in_item, 0])
                 tmp[in_item, 0] = item - count
-                result = np.append(result, tmp, axis=0)
+            result = np.append(result, tmp, axis=0)
         elif arr[item - 1] == 0:
             count += 1
     savetxt("dataset/new_interactions.csv", result, delimiter="\t", fmt="%d")
