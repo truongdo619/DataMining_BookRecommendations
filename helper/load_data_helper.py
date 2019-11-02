125ab14960b1c228b36f12216b249da4c6fbd7ea
truongdo619 <conmuonvenha@gmail.com>
2019-11-02 12:57:36 +0700

    update preparing_dataset

--- helper/load_data_helper.py
+++ helper/load_data_helper.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def load_csv_to_numpy(path):
    my_data = genfromtxt(path, delimiter='\t',dtype=int,skip_header=0)
-   # savetxt("ratings_small.csv", my_data[((my_data[:, 1] <= 2000) & (my_data[:, 0] <= 5000 ))], delimiter="\t", fmt="%d")
+   # savetxt("dataset/interactions.csv", my_data[((my_data[:, 1] <= 1000) & (my_data[:, 0] <= 1000 ))], delimiter="\t", fmt="%d")
    return my_data
 
-# data = load_csv_to_numpy("dataset/small_dataset/interactions_small.csv")
+# data = load_csv_to_numpy("dataset/interactions_6M.csv")
--- helper/preparing_dataset.py
+++ helper/preparing_dataset.py
@@ -4,27 +4,23 @@ import numpy as np
 from load_data_helper import load_csv_to_numpy
 import random
 
-def preparing(path):
+def preparing(path, threshold = 20):
     data = load_csv_to_numpy(path)
-    nb_line = data.shape[0]
     nb_user = np.amax(np.delete(data, 1, 1))
-    nb_item = np.amax(np.delete(data, 0, 1))
-    arr = np.zeros(shape=(nb_line,2), dtype=np.int16)
+    arr = [ item if data[data[:, 0] == item].shape[0] > threshold else 0 for item in range(1, nb_user+1) ]
+    result = None
     count = 0
-    while count < 100:
-        rand_user = random.randrange(1,nb_user+1)
-        rand_item = random.randrange(1,nb_item+1)
-        tmp = [rand_user, rand_item]
-        if not (data == np.array(tmp)).all(1).any() and not (arr == np.array(tmp)).all(1).any():
-            arr[count] = tmp
+    for item in range(1,nb_user+1):
+        if result is None:
+            result = data[data[:, 0] == item]
+        elif arr[item - 1] != 0:
+            tmp = data[data[:, 0] == item]
+            for in_item in range(tmp.shape[0]):
+                tmp[in_item, 0] = item - count
+                result = np.append(result, tmp, axis=0)
+        elif arr[item - 1] == 0:
             count += 1
-            print (count)
-    paths = path.split("/")
-    paths[len(paths) - 1] = "no" + paths[len(paths) - 1]
-    path = "/".join(paths)
-    savetxt(path, arr, delimiter="\t", fmt="%d")
-
+    savetxt("dataset/new_interactions.csv", result, delimiter="\t", fmt="%d")
 
 if __name__ == '__main__':
-    preparing("dataset/small_dataset/interactions_small.csv")
-    preparing("dataset/small_dataset/interactions_6M.csv")
+    preparing("dataset/interactions.csv")
